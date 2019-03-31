605cea13c995d3af3d459744f2732afcd40cfcc1
liy <liy8272@gmail.com>
2019-03-31 10:54:04 +0100

    Initial commit

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize Firebase
+var config = {
+  // You can get the sender id from: https://console.firebase.google.com/u/0/project/ao-speech-to-tex-1553895570081/settings/cloudmessaging/
+  // Used to validate the sender by firebase cloud messaging lib.
+  messagingSenderId: "412805037378"
+
+  // Also note that, gcm_sender_id is added into the manifest.json file
+  // It is completely different from messagingSenderId.
+  // I imagine it is identifier for the whole firebase cloud messaging service.
+  // You can read more about it here
+  // https://firebase.google.com/docs/cloud-messaging/js/client#configure_the_browser_to_receive_messages
+  // https://developer.clevertap.com/docs/find-your-gcm-sender-id-gcm-api-server-key
+};
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
--- src/index.js
+++ src/index.js
@@ -3,9 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from "firebase";
 
+// Initialize Firebase for rececing cloud messaging notification
+var config = {
+  apiKey: "AIzaSyDiUbOpgOlkH9Z5q9VYe0ETK5L1dbkcuOc",
+  authDomain: "ao-speech-to-tex-1553895570081.firebaseapp.com",
+  databaseURL: "https://ao-speech-to-tex-1553895570081.firebaseio.com",
+  projectId: "ao-speech-to-tex-1553895570081",
+  storageBucket: "ao-speech-to-tex-1553895570081.appspot.com",
+  messagingSenderId: "412805037378"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+// A template firebase-messaging-sw.js is in public folder.
+// When user app is not in focus or closed. Service worker will handle all the background messaging.
+
+// Request user permission to receive notifications
+messaging.requestPermission().then(async () => {
+  console.log('Notification permission granted.');
+  // Get device registration token. Initially this makes a network call, once retrieved
+  // subsequent calls to getToken will return from cache.
+  // The token is used to identify the physical devices, so serverside can send notification
+  // to specific devices.
+  const token = await messaging.getToken();
+  console.log('Copy paste the Registration token below to serverside cloud function for testing:')
+  console.log(`%c${token}`, `color:orange; background:grey`)
+}).catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+
+// When application is in focus, instead of notification from service worker, it will be handled here.
+messaging.onMessage(payload => {
+  console.log('message', payload)
+})
+
+
+// All the other react crap
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
